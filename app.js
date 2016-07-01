dff0b2e9a528fd95533b1681a8ba0370e317cd53
therrance <lozuvatka@gmail.com>
2016-07-01 16:13:58 +0300

    Add disable tabs

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
-angular.module('app', [])
-    .directive('tab', function () {
-        return {
-            restrict: 'E',
-            transclude: true,
-            template: '<div role="tabpanel" ng-show="active" ng-transclude></div>',
-            require: '^tabset',
-            scope: {
-                heading: '@'
-            },
-            link: function (scope, elem, attr, tabsetCtrl) {
-                scope.active = false;
-                tabsetCtrl.addTab(scope)
+;(function (window) {
+    angular.module('app', [])
+        .directive('tab', function () {
+            return {
+                restrict: 'E',
+                transclude: true,
+                template: '<div role="tabpanel" ng-show="active" ng-transclude></div>',
+                require: '^tabset',
+                scope: {
+                    heading: '@'
+                },
+                link: function (scope, elem, attr, tabsetCtrl) {
+                    scope.active = false;
+
+                    scope.disabled = false;
+                    if (attr.disable) {
+                        attr.$observe('disable', function (value) {
+                            scope.disabled = (value !== 'false');
+                        })
+                    }
+                    tabsetCtrl.addTab(scope)
+                }
             }
-        }
-    })
-    .directive('tabset', function () {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: {},
-            templateUrl: 'tabset.html',
-            bindToController: true,
-            controllerAs: 'tabset',
-            controller: function () {
-                var self = this;
-                self.tabs = [];
+        })
+        .directive('tabset', function () {
+            return {
+                restrict: 'E',
+                transclude: true,
+                scope: {},
+                templateUrl: 'tabset.html',
+                bindToController: true,
+                controllerAs: 'tabset',
+                controller: function () {
+                    var self = this;
+                    self.tabs = [];
 
-                self.addTab = function addTab(tab) {
-                    self.tabs.push(tab);
+                    self.addTab = function addTab(tab) {
+                        self.tabs.push(tab);
 
-                    if (self.tabs.length === 1) {
-                        tab.active = true
+                        if (self.tabs.length === 1) {
+                            tab.active = true
+                        }
+                    };
+
+                    self.select = function (selectedTab) {
+                        if (selectedTab) {return}
+
+                        angular.forEach(self.tabs, function (tab) {
+                            if (tab.active && tab !== selectedTab) {
+                                tab.active = false;
+                            }
+                        });
+
+                        selectedTab.active = true;
                     }
                 }
-
-                self.select = function (selectedTab) {
-                    angular.forEach(self.tabs, function (tab) {
-                        if (tab.active && tab !== selectedTab) {
-                            tab.active = false;
-                        }
-                    })
-
-                    selectedTab.active = true;
-                }
             }
-        }
-    })
+        });
+})(window);
